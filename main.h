b2f6ee05527e3bdbc24d38548a281d9aada332a4
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-26 19:02:42 +0200

    delete unessesary prototypes

--- main.h
+++ main.h
@@ -1,13 +1,4 @@
 #ifndef our_printf
 #define our_printf
-#include <stdio.h>
-#include <stdarg.h>
 int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
-int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
 #endif
