094961054f6844d54c9be8ce5726cf8f8b516240
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-26 04:54:28 +0200

    betty

--- _printf.c
+++ _printf.c
@@ -10,38 +10,38 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
+int count = 0;
+va_list args;
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-				{
-					char c = (char)va_arg(args, int);
-					count += write(1, &c, 1);
-					break;
-				}
-				case 's':
-					count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
-					break;
-				case '%':
-					count += write(1, "%", 1);
-					break;
-				default:
-					count += write(1, "%", 1) + write(1, format, 1);
-					break;
-			}
-		}
-		else
-			count += write(1, format, 1);
-		format++;
-	}
-	va_end(args);
-	return (count);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+	case 'c':
+{
+char c = (char)va_arg(args, int);
+count += write(1, &c, 1);
+break;
+}
+case 's':
+count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
+break;
+case '%':
+count += write(1, "%", 1);
+break;
+default:
+count += write(1, "%", 1) + write(1, format, 1);
+break;
+}
+}
+else
+count += write(1, format, 1);
+format++;
+}
+va_end(args);
+return (count);
 }
