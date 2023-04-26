3ebbcec85c2af035288e2716430fa3e444b22da8
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-26 18:43:01 +0200

    missing semi_colon *_*

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define our_printf
 #include <stdio.h>
 #include <stdarg.h>
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
