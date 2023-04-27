dab7e8b55e6cd70875b92569b2f21ea26807c16d
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-27 09:41:50 +0200

    break it down to more managable functions

--- _printf.c
+++ _printf.c
@@ -7,62 +7,91 @@
 #include "main.h"
 
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
- * Return: number of characters printed
+ * print_char - prints a character
+ * @c: the character to print
+ *
+ * Return: 1 (the number of characters printed)
+ */
+int print_char(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string
+ * @str: the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_string(char *str)
+{
+int len;
+
+if (str == NULL)
+str = "(null)";
+len = strlen(str);
+return (write(1, str, len));
+}
+
+/**
+ * print_integer - prints an integer
+ * @n: the integer to print
+ *
+ * Return: the number of characters printed
+ */
+int print_integer(int n)
+{
+int len = 0;
+unsigned int abs_val;
+
+if (n < 0)
+{
+len += print_char('-');
+abs_val = -n;
+}
+else
+abs_val = n;
+
+if (abs_val > 9)
+len += print_integer(abs_val / 10);
+len += print_char(abs_val % 10 + '0');
+return (len);
+}
+
+/**
+ * _printf - custom printf function
+ * @format: the format string
+ *
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 int count = 0;
 va_list args;
-char c;
 
-va_start(args, format);
 if (format == NULL)
 return (-1);
+va_start(args, format);
 while (*format)
 {
 if (*format == '%')
 {
 format++;
 if (*format == '\0')
-{
+ {
 count += write(1, "%", 1);
-return (1);
-}
-if (*format == '%')
-{
+break;
+ }
+else if (*format == '%')
 count += write(1, "%", 1);
-format++;
-continue;
-}
-switch (*format)
-{
-case 'c':
-{
-c = (char)va_arg(args, int);
-if (c == '\0')
-count += write(1, "\0", 1);
+else if (*format == 'c')
+count += print_char(va_arg(args, int));
+else if (*format == 's')
+count += print_string(va_arg(args, char *));
+else if (*format == 'd')
+count += print_integer(va_arg(args, int));
 else
-count += write(1, &c, 1);
-break;
-}
-case 's':
-{
-char *str = va_arg(args, char *);
-if (str == NULL)
-count += write(1, "(null)", 6);
-else
-count += write(1, str, strlen(str));
-}
-break;
-case '%':
-count += write(1, "%", 1);
-break;
-default:
 count += write(1, "%", 1) + write(1, format, 1);
-break;
-}
 }
 else
 count += write(1, format, 1);
